d74b7e6328d63f9627c44c85a46d880333207856
Ghasemi <mojtaba_ghasemi@outlook.com>
2023-06-12 14:29:23 +0330

    first commit

---
+++ src/main/java/com/azin/javaspringboothumanresource/JavaSpringBootHumanResourceApplication.java
@@ -0,0 +1,13 @@
+package com.azin.javaspringboothumanresource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootHumanResourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringBootHumanResourceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/azin/javaspringboothumanresource/JavaSpringBootHumanResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azin.javaspringboothumanresource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootHumanResourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
